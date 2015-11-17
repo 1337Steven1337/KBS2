8156c8f4397dd50395eb62eac12198f41bd2075b
Remco Schipper <github@remcoschipper.com>
2015-11-17 12:40:49 +0100

    First Commit

---
+++ Interactieve Docent/Server/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models
+{
+    public class Question
+    {
+        [Key]
+        public int id { get; set; }
+    }
+}
