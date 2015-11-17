46299a1a9e6584fdf03a187b705f407e747c2389
Remco Schipper <github@remcoschipper.com>
2015-11-17 15:13:46 +0100

    Begin of the API client in the server

---
+++ Interactieve Docent/Client/API/List.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.API
+{
+    class List
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public List()
+        {
+            this.Questions = new List<Question>();
+        }
+
+        public static List<List> getAll()
+        {
+            Api api = new Api();
+
+            var request = new RestRequest();
+            request.Resource = "Questions";
+
+            return api.Execute<List<List>>(request);
+        }
+    }
+}
---
+++ Interactieve Docent/Server/Models/DTO/ListDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models.DTO
+{
+    public class ListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
