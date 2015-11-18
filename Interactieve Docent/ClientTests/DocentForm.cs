3db2497d1c1a7dfd9dd40f71cfd70c738f413b22
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-11-18 11:59:22 +0100

    Create testclass

---
+++ Interactieve Docent/ClientTests/DocentForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientTests
+{
+    [TestClass]
+    public class DocentForm
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
