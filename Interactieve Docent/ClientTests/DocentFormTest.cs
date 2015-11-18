58feb632c10e2d1c0ce3d8974b30c9ef9a2077e6
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-11-18 12:16:27 +0100

    ValidateEmptyFields unittest

---
+++ Interactieve Docent/ClientTests/DocentFormTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientTests
+{
+    [TestClass]
+    public class DocentFormTest
+    {
+        [TestMethod]
+        public void ValidateEmptyFieldsSomeEmpty()
+        {
+            Client.DocentForm df = new DocentForm();
+            Boolean expected = true;
+            Boolean result = df.ValidateEmptyFields();
+            Assert.AreEqual(expected, result, "Fields are not displayed as empty!");
+        }
+
+        [TestMethod]
+        public void btnOpslaan_Click()
+        {
+            
+        }
+    }
+}
