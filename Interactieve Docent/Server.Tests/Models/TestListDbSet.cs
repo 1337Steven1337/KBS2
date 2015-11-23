70c1c29c86b8099f319819036339424b7c93487f
Remco Schipper <github@remcoschipper.com>
2015-11-23 13:26:16 +0100

    Unit testing applied to the list controller

---
+++ Interactieve Docent/Server.Tests/Models/TestListDbSet.cs
@@ -0,0 +1,17 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Tests.Models
+{
+    class TestListDbSet : TestDbSet<List>
+    {
+        public override List Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(list => list.Id == (int)keyValues.Single());
+        }
+    }
+}
