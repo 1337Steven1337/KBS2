e8bcf91fe0b5fa69d4ef18b12117f9f1d3ae2057
Johan <johan_luiken@hotmail.com>
2015-11-26 11:54:18 +0100

    Buttons

--- Interactieve Docent/Client/Student/Questions.Designer.cs
+++ Interactieve Docent/Client/Student/Questions.Designer.cs
@@ -30,34 +30,44 @@
         {
             this.questionLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // questionLabel
             // 
             this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.questionLabel.Location = new System.Drawing.Point(50, 50);
+            this.questionLabel.Location = new System.Drawing.Point(33, 32);
+            this.questionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.questionLabel.Name = "questionLabel";
-            this.questionLabel.Size = new System.Drawing.Size(668, 262);
+            this.questionLabel.Size = new System.Drawing.Size(445, 170);
             this.questionLabel.TabIndex = 0;
             this.questionLabel.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 375);
+            this.button1.Location = new System.Drawing.Point(0, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(891, 187);
+            this.button1.Size = new System.Drawing.Size(594, 122);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-88, 255);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1, 0);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
             // Questions
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 562);
+            this.ClientSize = new System.Drawing.Size(593, 365);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.questionLabel);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Questions";
             this.Text = "Questions";
             this.Load += new System.EventHandler(this.Questions_Load);
@@ -69,5 +79,6 @@
 
         private System.Windows.Forms.Label questionLabel;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Interactieve Docent/Client/Student/Questions.cs
+++ Interactieve Docent/Client/Student/Questions.cs
@@ -16,6 +16,7 @@ namespace Client.Student
 {
     public partial class Questions : Form
     {
+        public int percentageofHeigt { get; set; }
         private int List_Id { get; set; }
         private int currentQuestionIndex = -1;
         private bool busy = false;
@@ -23,7 +24,7 @@ namespace Client.Student
         static int ButtonCounterHorizontal = 0;
         static int ButtonCounterVertical = 1;
         private List<Button> answerButtons = new List<Button>();
-
+        private Button option = null;
         private ProgressBar timerProgressBar;
 
         private List list = null;
@@ -55,26 +56,51 @@ namespace Client.Student
 
         private Button createAnswerButton(PredefinedAnswer answer)
         {
-            Button option = new Button();
+
+
+            option = new Button();
+            answerButtons.Add(option);
             option.Text = answer.Text;
-            option.Location = new Point(Width / 2 + option.Width * (ButtonCounterHorizontal), Height - option.Height * (4 - ButtonCounterVertical));
-            ButtonCounter++;
-            if (ButtonCounter % 2 == 0)
+            answerButtons.Add(option);
+            //option.Location = new Point(Width / 2 + option.Width * (ButtonCounterHorizontal), Height - option.Height * (4 - ButtonCounterVertical));
+            return option;
+        }
+
+
+        private void adjustSizeOfButtons(PredefinedAnswer PA)
+        {
+
+            if (option == null)
             {
-                ButtonCounterVertical++;
-                ButtonCounter = 0;
+                option = createAnswerButton(PA);
             }
-            if (ButtonCounterHorizontal <= 2)
+            //bereken de grootte
+            int precentagePerButton = (int)Math.Ceiling((double)currentQuestion.PredefinedAnswers.Count / 2);
+            int heightcounter = 0;
+            int locationY = 0;
+            int locationX = 0;
+            int WorkingArea = 0;
+            float ButtonListCounter = 0;
+            //y locatie berekenen
+            ButtonListCounter = (int) Math.Floor((double)currentQuestion.PredefinedAnswers.Count/2);
+            percentageofHeigt = 30;
+            if (ButtonListCounter % 1 == 0)
             {
-                ButtonCounterHorizontal += 1;
+                locationX = 0;
             }
             else
             {
-                ButtonCounterHorizontal = 0;
+                locationX = Width / 2;
             }
 
+            WorkingArea = (Height/100)*30;
+            //ken de hoogte toe
+            option.Height = WorkingArea/precentagePerButton;
+            locationY = option.Height*(int)ButtonListCounter;
 
-            return option;
+            //heightcounter = (int)Math.Floor(ButtonListCounter - 0.5);
+            option.Location = new Point();
+
         }
 
         private void cleanUpPreviousQuestion()
@@ -101,7 +127,7 @@ namespace Client.Student
 
                 foreach (PredefinedAnswer pa in currentQuestion.PredefinedAnswers)
                 {
-                    createAnswerButton(pa);
+                    adjustSizeOfButtons(pa);
                 }
             }
             else if (list.Ended)
