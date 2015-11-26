db615c43a9d3a996e9e731bbfbc1935598a40799
Simon van Bennekom <simonvanbennekom@gmail.com>
2015-11-26 22:08:26 +0100

    kleine commit, aanpassing Main

---
+++ Interactieve Docent/Client/Factory/UserAnswerFactory.cs
@@ -0,0 +1,36 @@
+﻿using Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Client.Factory
+{
+    public class UserAnswerFactory : AbstractFactory
+    {
+        private const string resource = "PredefinedAnswers";
+
+        public void save(UserAnswer answer, Action<UserAnswer> callback)
+        {
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            values.Add("Question_Id", answer.Question_Id);
+            values.Add("PredefinedAnswer_Id", answer.PredefinedAnswer_Id);
+
+            this.save<UserAnswer>(values, resource, callback);
+        }
+
+        public void findById(int id, Action<UserAnswer> callback)
+        {
+            this.findById<UserAnswer>(id, resource, callback);
+        }
+
+        public void findAll(Control control, Action<List<UserAnswer>> callback)
+        {
+            this.findAll<UserAnswer>(resource, control, callback);
+        }
+
+        public void findAllAsync(Action<List<UserAnswer>> callback)
+        {
+            this.findAllAsync<UserAnswer>(resource, callback);
+        }
+    }
+}
---
+++ Interactieve Docent/Server/Controllers/QuestionListsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Server.Models;
+using Server.Models.Context;
+using Server.Models.DTO;
+
+namespace Server.Controllers
+{
+    public class QuestionListsController : ApiController
+    {
+        private IDocentAppContext db = new ServerContext();
+
+        public QuestionListsController() { }
+        public QuestionListsController(IDocentAppContext context)
+        {
+            this.db = context;
+        }
+
+        // GET: api/QuestionLists
+        public IQueryable<ListDTO> GetLists()
+        {
+            var QuestionLists = from q in db.QuestionLists
+                                select new ListDTO()
+                                {
+                                    Id = q.Id,
+                                    Name = q.Name,
+                                    Questions = q.Questions.Select(C => new QuestionDTO { Id = C.Id }).ToList<QuestionDTO>()
+                                };
+
+            return QuestionLists;
+        }
+
+        // GET: api/QuestionLists/5
+        [ResponseType(typeof(ListDTO))]
+        public ListDTO GetList(int id)
+        {
+            var lists = from q in db.QuestionLists
+                        where q.Id == id
+                        select new ListDTO()
+                        {
+                            Id = q.Id,
+                            Name = q.Name,
+                            Questions = q.Questions.Select(C => new QuestionDTO { Id = C.Id, Text = C.Text, Time = C.Time, Points = C.Points, List_Id = C.List_Id, PredefinedAnswers = (C.PredefinedAnswers.Select(V => new PredefinedAnswerDTO { Id = V.Id, Text = V.Text, Question_Id = V.Question.Id })).ToList<PredefinedAnswerDTO>() }).ToList<QuestionDTO>()
+                        };
+
+            return lists.FirstOrDefault(x => x.Id == x.Id);
+        }
+
+        // PUT: api/QuestionLists/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutQuestionList(int id, QuestionList questionList)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != questionList.Id)
+            {
+                return BadRequest();
+            }
+
+            db.MarkAsModified(questionList);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/QuestionLists
+        [ResponseType(typeof(QuestionList))]
+        public async Task<IHttpActionResult> PostQuestionList(QuestionList questionList)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.QuestionLists.Add(questionList);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = questionList.Id }, questionList);
+        }
+
+        // DELETE: api/QuestionLists/5
+        [ResponseType(typeof(QuestionList))]
+        public async Task<IHttpActionResult> DeleteQuestionList(int id)
+        {
+            QuestionList questionList = await db.QuestionLists.FindAsync(id);
+            if (questionList == null)
+            {
+                return NotFound();
+            }
+
+            db.QuestionLists.Remove(questionList);
+            await db.SaveChangesAsync();
+
+            return Ok(questionList);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool QuestionListExists(int id)
+        {
+            return db.QuestionLists.Count(e => e.Id == id) > 0;
+        }
+    }
+}
