af36a65d847e02425ac24dc4a3f6a8611124da8a
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-11-26 10:53:27 +0100

    MVC setup

---
+++ Interactieve Docent/Client/Model/QuestionList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class QuestionList
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public bool Ended { get; set; }
+
+        public List<Question> Questions = new List<Question>();
+    }
+}
