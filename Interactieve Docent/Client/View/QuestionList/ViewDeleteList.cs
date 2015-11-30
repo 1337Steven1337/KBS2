c60f45aec420a01b01da0495d9208683d9306cee
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-11-30 14:09:57 +0100

    ViewDeleteList

--- Interactieve Docent/Client/View/QuestionList/ViewDeleteList.cs
+++ Interactieve Docent/Client/View/QuestionList/ViewDeleteList.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace Client.View.QuestionList
 {
     public partial class ViewDeleteList : Form
-    {
+    { 
         public Boolean valid { get; set; }
 
         public ViewDeleteList()
