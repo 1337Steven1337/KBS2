402ca62f07280d9a317af8935fc8760a0300b5fd
Johan <johan_luiken@hotmail.com>
2015-12-02 11:48:43 +0100

    QuestionsUnittest

---
+++ Interactieve Docent/Client.Tests/Controllers/QuestionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Client.Tests.Controllers
+{
+    [TestClass]
+    public class QuestionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
