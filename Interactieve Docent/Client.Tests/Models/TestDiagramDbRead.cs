d1ef809613c35189447402aab59bf094fcb63cf5
Simon van Bennekom <simonvanbennekom@gmail.com>
2015-12-02 13:55:08 +0100

    commit

---
+++ Interactieve Docent/Client.Tests/Models/TestDiagramDbRead.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Controllers;
+using Server.Models;
+
+namespace Client.Tests.Models
+{
+    /// <summary>
+    /// Summary description for UserAnswer
+    /// </summary>
+    [TestClass]
+    public class TestDiagramDbRead
+    {
+        [TestMethod]
+        public async Task PostList_ShouldReturnSameId()
+        {
+            //var controller = new diagram();
+
+            //var item = GetTestList();
+
+            //var result = 
+            //    await controller.PostUserAnswer(item) as CreatedAtRouteNegotiatedContentResult<UserAnswer>;
+
+        }
+
+        private UserAnswer GetTestList()
+        {
+            return new UserAnswer() { Id = 1, Question_Id = 3, PredefinedAnswer_Id = 1 };
+        }
+    }
+}
