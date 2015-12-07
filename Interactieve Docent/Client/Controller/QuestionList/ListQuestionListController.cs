2c82fc2101c3a73c6daa65719419ac72685bd3cb
Simon van Bennekom <simonvanbennekom@gmail.com>
2015-12-07 15:56:54 +0100

    Add file

---
+++ Interactieve Docent/Client/Controller/QuestionList/ListQuestionListController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.View;
+using Client.View.QuestionList;
+
+namespace Client.Controller.QuestionList
+{
+    public class ListQuestionListController : IController
+    {
+
+        private IQuestionListView view;
+        //private
+
+        public ListQuestionListController(IView view) 
+        {
+            this.SetView(view);
+        }
+            
+        public IView GetView()
+        {
+            return this.view;
+        }
+
+        public void SetView(IView view)
+        {
+            this.view = (IQuestionListView)view;
+        }
+    }
+}
