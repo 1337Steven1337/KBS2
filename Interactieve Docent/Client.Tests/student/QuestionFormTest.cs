d930c15ee14ede3f16d6f54fcb150c3b5a952309
alexander ineke <alexander.ineke@hotmail.com>
2015-12-08 12:24:30 +0100

    unit test

---
+++ Interactieve Docent/Client.Tests/student/QuestionFormTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+namespace Client.Tests.student
+{
+    [TestClass]
+    public class QuestionFormTest
+    {
+        Student.QuestionForm test = new Student.QuestionForm();
+
+        [TestMethod]
+        public void getProgressBarTest()
+        {
+        }
+        [TestMethod]
+        public void getQuestionListTest()
+        {
+            Assert.IsInstanceOfType(test.getQuestionList(), typeof(Model.QuestionList));
+        }
+        [TestMethod]
+        public void isBusyTest()
+        {
+            bool expected = false;
+
+            bool result = test.isBusy();
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void getCurrentQuestionTest()
+        {
+
+        }
+        [TestMethod]
+        public void goToNextQuestionTest()
+        {
+
+        }
+
+
+    }
+}
