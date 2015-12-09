ca4a428de988d2dba015719aeeb3a94eceb64437
Remco Schipper <github@remcoschipper.com>
2015-12-09 12:14:00 +0100

    Added begin of the generating of the user accounts

---
+++ Interactieve Docent/Client/Controller/Account/AddAccountController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Factory;
+using Client.Model;
+using Client.View;
+using Excel;
+using Client.View.Account;
+using System.Security.Cryptography;
+using System.Net;
+using RestSharp;
+using Client.Service.Email;
+
+namespace Client.Controller.Account
+{
+    public class AddAccountController : AbstractController<Model.Account>
+    {
+        static readonly char[] AvailableCharacters = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        };
+
+        private IAddAccountView View { get; set; }
+        private AccountFactory Factory = new AccountFactory();
+        private List<String> UsedPasswords = new List<String>();
+        private IEmailClient EmailClient { get; set; }
+
+        public AddAccountController(IAddAccountView view) : this(view, new EmailClient())
+        {
+
+        }
+        public AddAccountController(IAddAccountView view, IEmailClient client)
+        {
+            this.EmailClient = new EmailClient();
+            this.View = view;
+            this.View.SetController(this);
+        }
+
+        private void AccountSaved(Model.Account account, HttpStatusCode code, IRestResponse response)
+        {
+            if(code == HttpStatusCode.Created)
+            {
+
+            }
+            else
+            {
+                this.View.ShowSaveFailed();
+            }
+        }
+
+        private string Sha256(string value)
+        {
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                return String.Join("", hash
+                  .ComputeHash(Encoding.UTF8.GetBytes(value))
+                  .Select(item => item.ToString("x2")));
+            }
+        }
+
+        private string GeneratePassword(int length)
+        {
+            char[] identifier = new char[length];
+            byte[] randomData = new byte[length];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomData);
+            }
+
+            for (int idx = 0; idx < identifier.Length; idx++)
+            {
+                int pos = randomData[idx] % AvailableCharacters.Length;
+                identifier[idx] = AvailableCharacters[pos];
+            }
+
+            string password = new string(identifier);
+
+            if(this.UsedPasswords.Contains(password))
+            {
+                password = this.GeneratePassword(length);
+            }
+
+            this.UsedPasswords.Add(password);
+
+            return this.Sha256(password);
+        }
+
+        private void ProcessUser(Row row)
+        {
+            Model.Account account = new Model.Account();
+            account.Number = row.Cells[1].Text.ToLower();
+            account.Password = this.GeneratePassword(5);
+
+            this.Factory.Save(account, this.View.GetHandler(), this.AccountSaved);
+        }
+
+        public void ProcessFile(string path)
+        {
+            UsedPasswords.Clear();
+            IEnumerable<worksheet> sheets = Workbook.Worksheets(path);
+
+            if(sheets.Count() > 0)
+            {
+                foreach (worksheet sheet in sheets)
+                {
+                    IEnumerable<Row> rows = sheet.Rows;
+
+                    for(int i = 1; i < rows.Count(); i++)
+                    {
+                        this.ProcessUser(rows.ElementAt(i));
+                    }
+                }
+            }
+            else
+            {
+                this.View.ShowLoadFailed();
+            }
+        }
+
+        public override IView GetView()
+        {
+            return this.View;
+        }
+
+        public override void SetBaseFactory(IFactory<Model.Account> baseFactory)
+        {
+            this.Factory.SetBaseFactory(baseFactory);
+        }
+
+        public override void SetView(IView view)
+        {
+            this.View = (IAddAccountView)view;
+        }
+    }
+}
---
+++ Interactieve Docent/Client/View/Account/AddAccountView.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows.Forms;
+using Client.Controller;
+using Client.Model;
+using Client.Service.Thread;
+using Client.Controller.Account;
+using Client.View.Dialogs;
+
+namespace Client.View.Account
+{
+    public partial class AddAccountView : Form, IAddAccountView
+    {
+        private AddAccountController Controller { get; set; }
+
+        public AddAccountView()
+        {
+            InitializeComponent();
+
+            this.SelectExcelFileDialog.FileOk += SelectExcelFileDialog_FileOk;
+        }
+
+        private void SelectExcelFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if(!e.Cancel)
+            {
+                this.Controller.ProcessFile(this.SelectExcelFileDialog.FileName);
+            }
+        }
+
+        public void AddToParent(IView parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IControlHandler GetHandler()
+        {
+            return new ControlHandler(this.OpenDialogButton);
+        }
+
+        public PredefinedAnswer GetSelectedAnswer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetController(IController controller)
+        {
+            this.Controller = (AddAccountController)controller;
+        }
+
+        public void ShowSaveFailed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowSaveResult(Model.Account instance, HttpStatusCode status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowSaveSucceed()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenDialogButton_Click(object sender, EventArgs e)
+        {
+            this.SelectExcelFileDialog.ShowDialog();
+
+        }
+
+        public void ShowLoadFailed()
+        {
+            FailedDialogView failed = new FailedDialogView();
+            failed.getLabelFailed().Text = "Het Excel bestand is ongeldig, er is geen sheet gevonden.";
+            failed.ShowDialog();
+        }
+    }
+}
