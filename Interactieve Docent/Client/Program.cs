d27d203c3ebae95c2f8d93f5247a1e4de1f2519c
alexander ineke <alexander.ineke@hotmail.com>
2015-12-14 13:15:32 +0100

    program

--- Interactieve Docent/Client/Program.cs
+++ Interactieve Docent/Client/Program.cs
@@ -30,19 +30,19 @@ namespace Client
 
             
             
-            MainView view = new MainView();
-            MainController maincontroller = new MainController(view);
+            //MainView view = new MainView();
+            //MainController maincontroller = new MainController(view);
 
-            ListQuestionView viewQuestion = new ListQuestionView();
-            ListQuestionController questionController = new ListQuestionController(viewQuestion);
-            maincontroller.AddController(questionController);
+            //ListQuestionView viewQuestion = new ListQuestionView();
+            //ListQuestionController questionController = new ListQuestionController(viewQuestion);
+            //maincontroller.AddController(questionController);
 
-            ListQuestionListView viewQuestionList = new ListQuestionListView();
-            ListQuestionListController listQuestionListController = new ListQuestionListController(viewQuestionList);
-            maincontroller.AddController(listQuestionListController);
+            //ListQuestionListView viewQuestionList = new ListQuestionListView();
+            //ListQuestionListController listQuestionListController = new ListQuestionListController(viewQuestionList);
+            //maincontroller.AddController(listQuestionListController);
 
-            listQuestionListController.SelectedListChanged += questionController.LoadList;
-            listQuestionListController.Load();
+            //listQuestionListController.SelectedListChanged += questionController.LoadList;
+            //listQuestionListController.Load();
 
           //  AuthorisationView view = new AuthorisationView();
           //  AuthorisationController controller = new AuthorisationController(view);
@@ -50,7 +50,7 @@ namespace Client
            // AddAccountView view = new AddAccountView();
            // AddAccountController controller = new AddAccountController(view);
              
-            Application.Run(view);
+            Application.Run(new Student.QuestionForm(1));
         }
     }
 }
