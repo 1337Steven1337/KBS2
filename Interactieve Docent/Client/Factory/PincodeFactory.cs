5aa9916562a68e7288e68ef57cd8d1308c93b1d9
Remco Schipper <github@remcoschipper.com>
2015-12-16 12:51:59 +0100

    Added SignalR changes to the client

---
+++ Interactieve Docent/Client/Factory/PincodeFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Model;
+using Client.Service.Thread;
+
+namespace Client.Factory
+{
+    public class PincodeFactory : AbstractFactory<Model.Pincode>
+    {
+        protected override string Resource
+        {
+            get
+            {
+                return "Pincodes";
+            }
+        }
+
+        public PincodeFactory() : base(new BaseFactory<Model.Pincode>())
+        {
+
+        }
+       
+        protected override Dictionary<string, object> GetFields(Pincode instance)
+        {
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+            dictionary.Add("Code", instance.Code);
+
+            return dictionary;
+        }
+    }
+}
