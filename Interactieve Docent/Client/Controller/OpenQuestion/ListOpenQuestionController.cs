2178ba08513220a33839ed103f59b49e9bc38549
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-12-16 15:07:12 +0100

    ControllerUpdate

--- Interactieve Docent/Client/Controller/OpenQuestion/ListOpenQuestionController.cs
+++ Interactieve Docent/Client/Controller/OpenQuestion/ListOpenQuestionController.cs
@@ -6,11 +6,47 @@ using System.Threading.Tasks;
 using Client.Factory;
 using Client.Model;
 using Client.View;
+using System.Net;
 
 namespace Client.Controller.OpenQuestion
 {
     class ListOpenQuestionController : AbstractController<Model.OpenQuestion>
     {
+        #region Instances
+        private IListView<Model.OpenQuestion> View { get; set; }
+        private OpenQuestionFactory Factory = new OpenQuestionFactory();
+        #endregion
+
+        #region Constructors
+        public ListOpenQuestionController(IView view)
+        {
+            this.SetView(view);
+            this.View.SetController(this);
+        }
+        #endregion
+
+        #region Event
+        public void QuestionAdded(Model.OpenQuestion openQuestion)
+        {
+            this.View.AddItem(openQuestion);
+        }
+        #endregion
+
+        #region Methods
+        private void FillList(List<Model.OpenQuestion> openQuestions, HttpStatusCode status)
+        {
+            if (status == HttpStatusCode.OK && openQuestions != null)
+            {
+                this.View.FillList(openQuestions);
+            }
+        }
+
+        public void LoadList(Model.QuestionList list)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
         public override IView GetView()
         {
             throw new NotImplementedException();
