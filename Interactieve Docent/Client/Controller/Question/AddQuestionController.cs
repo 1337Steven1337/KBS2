2004ba73ef7a0d2f37bdd04a675f6271b1308e2e
Erwin Kok <s1079663@student.windesheim.nl>
2015-12-17 13:23:44 +0100

    update predefined Answers

--- Interactieve Docent/Client/Controller/Question/AddQuestionController.cs
+++ Interactieve Docent/Client/Controller/Question/AddQuestionController.cs
@@ -31,6 +31,8 @@ namespace Client.Controller.Question
         private Model.Question CurrentQuestion;
         private QuestionFactory Factory = new QuestionFactory();
         private Dictionary<string, int> AnswersSaved = new Dictionary<string, int>();
+        private Dictionary<string, int> AnswersDeleted = new Dictionary<string, int>();
+
         #endregion
 
         #region Constructors
@@ -53,13 +55,13 @@ namespace Client.Controller.Question
             this.View.ShowSaveResult(question, status);
         }
 
-        //private void CallbackUpdateQuestion(Model.Question question, HttpStatusCode status)
-        //{
-        //    if(status == HttpStatusCode.OK && question != null)
-        //    {
-        //        if()
-        //    }
-        //}
+        private void CallbackUpdateQuestion(Model.Question question, HttpStatusCode status)
+        {
+            if (status == HttpStatusCode.NoContent && question != null)
+            {
+                this.View.ShowUpdateResult(question, status);
+            }
+        }
 
         public override void SetBaseFactory(IFactory<Model.Question> factory)
         {
@@ -77,7 +79,8 @@ namespace Client.Controller.Question
             data.Add("List_Id", this.Parent.Id);
 
             Model.Question question = new Model.Question(data);
-            Factory.Update(question, this.View.GetHandler(), this.CallbackSaveQuestion);
+            question.Id = Convert.ToInt32(data["Id"]);
+            Factory.Update(question, this.View.GetHandler(), this.CallbackUpdateQuestion);
         }
 
         public void SaveQuestion(Dictionary<string, object> data)
@@ -86,6 +89,50 @@ namespace Client.Controller.Question
 
             Model.Question question = new Model.Question(data);
             Factory.Save(question, this.View.GetHandler(), this.CallbackSaveQuestion);
+        }
+
+        public void DeletePredefinedAnswers(List<Model.PredefinedAnswer> answers, Model.Question question)
+        {
+            this.DeletePredefinedAnswers(answers, question, new BaseFactory<PredefinedAnswer>());
+        }
+
+        public void DeletePredefinedAnswers(List<Model.PredefinedAnswer> answers, Model.Question question, IFactory<Model.PredefinedAnswer> baseFactory)
+        {
+            this.AnswersSaved.Clear();
+            //this.CurrentQuestion = question;
+
+            PredefinedAnswerFactory factory = new PredefinedAnswerFactory();
+            factory.SetBaseFactory(baseFactory);
+
+            foreach (Model.PredefinedAnswer answer in answers)
+            {
+                factory.Delete(answer, this.View.GetHandler(), CallbackDeletePredefinedAnswers);
+            }
+        }
+
+        private void CallbackDeletePredefinedAnswers(PredefinedAnswer predefinedAnswer, HttpStatusCode status)
+        {
+            if (status == HttpStatusCode.Created && predefinedAnswer != null)
+            {
+                AnswersDeleted[predefinedAnswer.Text] = 1;
+            }
+            else
+            {
+                AnswersDeleted[predefinedAnswer.Text] = 2;
+            }
+
+            if (!AnswersDeleted.ContainsValue(0))
+            {
+                if (AnswersDeleted.ContainsValue(2))
+                {
+                    //this.Factory.DeleteAsync(this.CurrentQuestion);
+                    this.View.ShowSaveFailed();
+                }
+                else
+                {
+                    this.View.ShowSaveSucceed();
+                }
+            }
         }
 
         public void SavePredefinedAnswers(List<Model.PredefinedAnswer> answers, Model.Question question)
@@ -128,15 +175,6 @@ namespace Client.Controller.Question
             if (status == HttpStatusCode.Created && predefinedAnswer != null)
             {
                 AnswersSaved[predefinedAnswer.Text] = 1;
-
-                /*
-                if(this.CurrentQuestion.PredefinedAnswers == null)
-                {
-                    this.CurrentQuestion.PredefinedAnswers = new List<PredefinedAnswer>();
-                }
-
-                this.CurrentQuestion.PredefinedAnswers.Add(predefinedAnswer);
-                */
             }
             else
             {
