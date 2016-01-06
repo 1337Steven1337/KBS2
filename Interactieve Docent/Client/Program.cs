ad0f46b7aeb176b2e40f0f1720faf2f3bdd3469b
Steven <coolesteven_8@hotmail.com>
2016-01-06 12:29:03 +0100

    Program overwrite

--- Interactieve Docent/Client/Program.cs
+++ Interactieve Docent/Client/Program.cs
@@ -30,26 +30,26 @@ namespace Client
             Application.SetCompatibleTextRenderingDefault(false);
 
 
-            //MainView view = new MainView();
-            //MainController maincontroller = new MainController(view);
+            MainView view = new MainView();
+            MainController maincontroller = new MainController(view);
 
-            //ListQuestionView viewQuestion = new ListQuestionView();
-            //ListQuestionController questionController = new ListQuestionController(viewQuestion);
-            //maincontroller.AddController(questionController);
+            ListQuestionView viewQuestion = new ListQuestionView();
+            ListQuestionController questionController = new ListQuestionController(viewQuestion);
+            maincontroller.AddController(questionController);
 
-            //ListQuestionListView viewQuestionList = new ListQuestionListView();
-            //ListQuestionListController listQuestionListController = new ListQuestionListController(viewQuestionList);
-            //maincontroller.AddController(listQuestionListController);
+            ListQuestionListView viewQuestionList = new ListQuestionListView();
+            ListQuestionListController listQuestionListController = new ListQuestionListController(viewQuestionList);
+            maincontroller.AddController(listQuestionListController);
 
-            //listQuestionListController.SelectedListChanged += questionController.LoadList;
-            //listQuestionListController.Load();
+            listQuestionListController.SelectedListChanged += questionController.LoadList;
+            listQuestionListController.Load();
 
 
-            //listQuestionListController.SelectedListChanged += questionController.LoadList;
-            //listQuestionListController.Load();
+            listQuestionListController.SelectedListChanged += questionController.LoadList;
+            listQuestionListController.Load();
 
-            //Student.QuestionForm form = new Student.QuestionForm(1);
-            //form.Show();
+            view.Show();
+
 
             StartView startView = new StartView();
             ShowStartController controller = new ShowStartController(startView);
