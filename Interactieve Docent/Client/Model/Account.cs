4cece0629c0f6fca21233abdff549a3baf258bf5
Remco Schipper <github@remcoschipper.com>
2016-01-07 13:30:25 +0100

    Fix git push

--- Interactieve Docent/Client/Model/Account.cs
+++ Interactieve Docent/Client/Model/Account.cs
@@ -9,7 +9,8 @@ namespace Client.Model
     public class Account : AbstractModel
     {
         public override int Id { get; set; }
-        public string Student { get; set; }
+        public string Student { get; set; } 
         public string Password { get; set; }
+        public string Token { get; set; }
     }
 }
